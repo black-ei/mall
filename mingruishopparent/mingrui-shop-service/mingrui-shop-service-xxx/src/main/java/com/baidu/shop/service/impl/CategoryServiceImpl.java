9488eef21dd2f5131148059d5b3b8e2805e0e598
black-ei <76053043+black-ei@users.noreply.github.com>
2021-01-04 16:30:59 +0800

    Update CategoryServiceImpl.java

--- mingruishopparent/mingrui-shop-service/mingrui-shop-service-xxx/src/main/java/com/baidu/shop/service/impl/CategoryServiceImpl.java
+++ mingruishopparent/mingrui-shop-service/mingrui-shop-service-xxx/src/main/java/com/baidu/shop/service/impl/CategoryServiceImpl.java
@@ -70,19 +70,20 @@ public class CategoryServiceImpl extends BaseApiService implements CategoryServi
         //判断当前节点是否为父节点
         if (categoryEntity.getIsParent()==1) return this.setResultError("当前节点为父节点,不能删除!");
 
-        //判断是否为根节点,根节点不能被删除
-        if(categoryEntity.getParentId()==0) return this.setResultError("当前节点为根节点无法删除!");
-        //判断当前节点的父节点是否有其他子节点
-        Example example = new Example(CategoryEntity.class);
-        example.createCriteria().andEqualTo("parentId", categoryEntity.getParentId());
-        List<CategoryEntity> entityList = categoryMapper.selectByExample(example);
-
-        //判断是否有品牌关联分类表
+        
+        
+         //判断是否有品牌关联分类表
         Example example1 = new Example(CategoryBrand.class);
         example1.createCriteria().andEqualTo("categoryId",id);
         List<CategoryBrand> categoryBrands = categoryBrandMapper.selectByExample(example1);
         if(categoryBrands.size()>0)return this.setResultError("当前节点有品牌绑定,请先删除品牌");
 
+        //判断当前节点的父节点是否有其他子节点
+        Example example = new Example(CategoryEntity.class);
+        example.createCriteria().andEqualTo("parentId", categoryEntity.getParentId());
+        List<CategoryEntity> entityList = categoryMapper.selectByExample(example);
+
+       
 
         //当前节点的父节点只有当前节点,将当前节点的父节(删除后当前节点后就不是子节点而变成叶子节点)把isParent改为0
         if(entityList.size()<=1){
